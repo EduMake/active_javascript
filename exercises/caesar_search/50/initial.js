a2c262951a09878282273939229c858ac7663585
Martyn Eggleton <martyn.eggleton@gmail.com>
2015-07-01 12:56:17 +0100

    Adds an A* level to the Caesar Search exercise

---
+++ exercises/caesar_search/50/initial.js
@@ -0,0 +1,26 @@
+/*global PlainTextAlphabet Decrypt */ //The PlainTextAlphabet and Decrypt functions are already written 
+
+//Checks whether the PlainText we have produced contains common words 
+//Returns true if found false if not
+function PlainTextContainsWords(PlainText) {
+
+
+}
+
+//Returns the CypherAlphabet (the outer ring) for the given Setting
+var MakeCaesarCypherAlphabet = function(Setting) {
+    //PlainTextAlphabet is the normal Alphabet ABCD....
+
+
+};
+
+//Trys to find a Setting which Decrypts the CypherText into PlainText which passes PlainTextContainsWordsing
+//Returns the Setting number if successful and false if it couldn't find anything
+function FindNextPossibleSetting(StartSetting, CypherText) {
+    //PlainTextAlphabet is the normal Alphabet ABCD....
+
+    // You will want to use the following functions :-
+        // Decrypt(PlainTextAlphabet, SettingAlphabet, CypherText);
+        // PlainTextContainsWords(PlainText); //Does the PlainText have reconginable words in it
+    
+}
