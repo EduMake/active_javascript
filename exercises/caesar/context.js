ba6ab15f8e367f95f4f71a9e9944a6b4c6b764ae
Martyn Eggleton <martyn.eggleton@gmail.com>
2015-07-06 23:09:21 +0100

    move the functions around and make sure they work

--- exercises/caesar/context.js
+++ exercises/caesar/context.js
@@ -1,77 +1,59 @@
 $("#matchesfound").hide();
-    
-    var autoAction = function() {
-        var sMode = $("#mode").val();
-        if(sMode == "Encrypt") {
-            getEncrypted();
-        } else if(sMode == "Decrypt") {
-            getDecrypted();
-        }
-    };
-    
-    var makeCaesarCypher = function() {
-        var sAlphabet = $("#alphabet").text();
-        var iCaesarSetting = $("#caesar_setting").val() % sAlphabet.length;
-        var sStart =  sAlphabet.slice(0, iCaesarSetting);
-        var sEnd =  sAlphabet.slice(iCaesarSetting);
-        var sNew = sEnd + sStart;
-        $("#cypheralphabet").text(sNew);
-        autoAction();
-    };
-    
-//CODE//    
-    
-    var getEncrypted = function() {
-        var sIn = $("#plaintext").val();
-        var sAlphabet = $("#alphabet").text(); 
-        var sCypher = $("#cypheralphabet").text();
-        var sCypherText = "";
-        if(sAlphabet.length !== sCypher.length)
-        {
-            sCypherText = "Cypher Too Short";
-        } else {
-            sCypherText = Encrypt(sAlphabet, sCypher, sIn);
-        }
-        $("#cyphertext").val(sCypherText);    
-    };
 
-    var getDecrypted = function() {
-        var sAlphabet = $("#alphabet").text(); 
-        var sIn = $("#cyphertext").val();
-        var sCypher = $("#cypheralphabet").text();
-        var sPlainText = Decrypt(sAlphabet, sCypher, sIn);
-        $("#plaintext").val(sPlainText);
-        //testPlainText();
-    };
+//CODE//    
+
+var getEncrypted = function() {
+    var sIn = $("#plaintext").val();
+    var sAlphabet = $("#alphabet").text(); 
+    var sCypher = $("#cypheralphabet").text();
+    var sCypherText = "";
+    if(sAlphabet.length !== sCypher.length)
+    {
+        sCypherText = "Cypher Too Short";
+    } else {
+        sCypherText = Encrypt(sAlphabet, sCypher, sIn);
+    }
+    $("#cyphertext").val(sCypherText);    
+};
+
+var getDecrypted = function() {
+    var sAlphabet = $("#alphabet").text(); 
+    var sIn = $("#cyphertext").val();
+    var sCypher = $("#cypheralphabet").text();
+    var sPlainText = Decrypt(sAlphabet, sCypher, sIn);
+    $("#plaintext").val(sPlainText);
+};
+
+var autoAction = function() {
+    var sMode = $("#mode").val();
+    if(sMode === "Encrypt") {
+        getEncrypted();
+    } else if(sMode === "Decrypt") {
+        getDecrypted();
+    }
+};
+
+var makeCaesarCypher = function() {
+    var sAlphabet = $("#alphabet").text();
+    var iCaesarSetting = $("#caesar_setting").val() % sAlphabet.length;
+    var sStart =  sAlphabet.slice(0, iCaesarSetting);
+    var sEnd =  sAlphabet.slice(iCaesarSetting);
+    var sNew = sEnd + sStart;
+    $("#cypheralphabet").text(sNew);
+    autoAction();
+};
     
-    var testPlainText = function () {
-        var aWords = $("#testwords").val().split(" ");
-        var oReg = new RegExp(aWords.join("|"));
-        var sPlainText = $("#plaintext").val();
-        
-        if(oReg.test(sPlainText)) {
-           $("#matchesfound").show();
-           return true;
-        } else {
-           $("#matchesfound").hide();
-           return false;
-        } 
-    };
-    
-    $("#caesar_setting").on("input", makeCaesarCypher);
-    
-    $("#plaintext").on("input", getEncrypted);
-    
-    $("#plaintext").on("input", getEncrypted);
-    $("#cyphertext").on("input", getDecrypted);
-    
-    $("#cypheralphabet").on("input", autoAction);
-    
-    $(".getEncrypted").click(getEncrypted);
-    $(".decrypt").click(getDecrypted);
-    
-    makeCaesarCypher();
+$("#caesar_setting").on("input", makeCaesarCypher);
+
+$("#plaintext").on("input", getEncrypted);
+$("#cyphertext").on("input", getDecrypted);
+
+$("#cypheralphabet").on("input", autoAction);
+
+$(".encrypt").click(getEncrypted);
+$(".decrypt").click(getDecrypted);
+
+makeCaesarCypher();
     
 //TESTS//
-    
-    
+
