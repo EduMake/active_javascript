36b43d2b28231401890ed92d7ade0b5240693d0b
Martyn Eggleton <martyn.eggleton@gmail.com>
2015-10-07 16:45:55 +0100

    same as flowchart etc.

--- exercises/caesar/45/initial.js
+++ exercises/caesar/45/initial.js
@@ -2,8 +2,8 @@ var Encrypt = function(PlainAlphabet, CypherAlphabet, PlainText) {
     var CypherText = ""; //We start with no letters in our output
     var Position = 0;
     while(Position < PlainText.length ) { 
-        var PlainTextLetter = PlainText[Position]; //The letter we want to Encypher this time
-        var LetterNum = PlainAlphabet.search(PlainTextLetter); //Finds what position our Letter is in our Alphabet
+        var Letter = PlainText[Position]; //The letter we want to Encypher this time
+        var LetterNum = PlainAlphabet.search(Letter); //Finds what position our Letter is in our Alphabet
         CypherText = CypherText + CypherAlphabet[LetterNum];//Look up that position in our Substitution Alphabet and add our Cypher Letter to the OutputText
         Position = Position + 1;
     }
