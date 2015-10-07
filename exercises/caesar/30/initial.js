1d8537c97770bcde69952ef752783d0507408153
Martyn Eggleton <martyn.eggleton@gmail.com>
2015-10-07 16:41:26 +0100

    Closer to Flow Diagram & Psuedocode

--- exercises/caesar/30/initial.js
+++ exercises/caesar/30/initial.js
@@ -2,8 +2,8 @@ var Encrypt = function(PlainAlphabet, CypherAlphabet, PlainText) {
     var CypherText = ""; //We start with no letters in our output
     var Position = 0;
     while(Position < PlainText.length ) { 
-        var PlainTextLetter = PlainText[Position]; //The letter we want to Encypher this time
-        var LetterNum = PlainAlphabet.search(PlainTextLetter); //Finds what position our Letter is in our Alphabet
+        var Letter = PlainText[Position]; //The letter we want to Encypher this time
+        var LetterNum = PlainAlphabet.search(Letter); //Finds what position our Letter is in our Alphabet
         CypherText = CypherText + CypherAlphabet[LetterNum];//Look up that position in our Substitution Alphabet and add our Cypher Letter to the OutputText
         Position = Position + 1;
     }
@@ -14,8 +14,8 @@ var Decrypt = function(PlainAlphabet, CypherAlphabet, CypherText) {
     var PlainText = ""; //We start with no letters in our output
     var Position = 0;
     while(Position < CypherText.length ) { 
-        var CypherTextLetter = CypherText[Position]; //The letter we want to EnPlain this time
-        var LetterNum = CypherAlphabet.search(PlainTextLetter); //Finds what position our Letter is in our Alphabet
+        var Letter = CypherText[Position]; //The letter we want to Decrypt this time
+        var LetterNum = CypherAlphabet.search(Letter); //Finds what position our Letter is in our Alphabet
         PlainText = PlainText + PlainAlphabet[LetterNum];//Look up that position in our Substitution Alphabet and add our Plain Letter to the OutputText
         Position = Position + 2;
     }
