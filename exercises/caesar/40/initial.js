ca5873ccced77a1c216faf01062bf45272217627
Martyn Eggleton <martyn.eggleton@gmail.com>
2015-10-07 02:56:21 +0100

    stops at end of exercise and makes caesarlooklike pseudo code

--- exercises/caesar/40/initial.js
+++ exercises/caesar/40/initial.js
@@ -1,28 +1,23 @@
-var Encrypt = function(Alphabet, SubstitutionAlphabet, PlainText) {
-    var OutputText = ""; //We start with no letters in our output
-    
-    //The variable i will go up from 0 (pointing to the first letter)
-    //until i = the length of our plain text (pointing to the last letter in our plain text)
-    for(var i = 0 ; i < PlainText.length; i++) { 
-        var PlainTextLetter = PlainText[i]; //The letter we want to Encypher this time
-        var Position = Alphabet.search(PlainTextLetter); //Finds what position our Letter is in our Alphabet
-        if(Position === -1) { //If we didn't find the letter in our Alphabet
-            Position = Alphabet.search("_"); //Find "_" instead
-        }
-        var CypherLetter = SubstitutionAlphabet[Position]; //Look up that position in our Substitution Alphabet
-        OutputText += CypherLetter; //Add our Cypher Letter to the OutputText
+var Encrypt = function(PlainAlphabet, CypherAlphabet, PlainText) {
+    var CypherText = ""; //We start with no letters in our output
+    var Position = 0;
+    while(Position < PlainText.length ) { 
+        var PlainTextLetter = PlainText[Position]; //The letter we want to Encypher this time
+        var LetterNum = PlainAlphabet.search(PlainTextLetter); //Finds what position our Letter is in our Alphabet
+        CypherText = CypherText + CypherAlphabet[LetterNum];//Look up that position in our Substitution Alphabet and add our Cypher Letter to the OutputText
+        Position = Position + 1;
     }
-    return OutputText; //Send it back
+    return CypherText; //Send it back
 };
 
-var Decrypt = function(Alphabet, SubstitutionAlphabet, CypherText) {
-    var OutputText = ""; //We start with no letters in our output
-    for(var i = 0 ; i < CypherText.length; i++) { 
-                   //The letter we want to Encypher this time
-        var Position = SubstitutionAlphabet.search(CypherTextLetter); //Finds what position our Letter is in our SubstitutionAlphabet
-                   //Look up that position in our Plain text Alphabet
-        OutputText += PlainTextLetter; //Add our Plain Text Letter to the OutputText
+var Decrypt = function(PlainAlphabet, CypherAlphabet, CypherText) {
+    var PlainText = ""; //We start with no letters in our output
+    var Position = 0;
+    while(Position < CypherText.length ) { 
+        var             = CypherText[Position]; //The letter we want to Decrypt this time
+        var LetterNum = PlainAlphabet.search(CypherTextLetter); //Finds what position our Letter is in our Alphabet
+        PlainText = PlainText +              ;//Look up that position in our Substitution Alphabet and add our Plain Letter to the OutputText
+        Position = Position + 1;
     }
-    return OutputText; //Send it back
+    return PlainText; //Send it back
 };
-
--- exercises/caesar/45/initial.js
+++ exercises/caesar/45/initial.js
@@ -1,25 +1,20 @@
-var Encrypt = function(Alphabet, SubstitutionAlphabet, PlainText) {
-    var OutputText = ""; //We start with no letters in our output
-    
-    //The variable i will go up from 0 (pointing to the first letter)
-    //until i = the length of our plain text (pointing to the last letter in our plain text)
-    for(var i = 0 ; i < PlainText.length; i++) { 
-        var PlainTextLetter = PlainText[i]; //The letter we want to Encypher this time
-        var Position = Alphabet.search(PlainTextLetter); //Finds what position our Letter is in our Alphabet
-        if(Position === -1) { //If we didn't find the letter in our Alphabet
-            Position = Alphabet.search("_"); //Find "_" instead
-        }
-        var CypherLetter = SubstitutionAlphabet[Position]; //Look up that position in our Substitution Alphabet
-        OutputText += CypherLetter; //Add our Cypher Letter to the OutputText
+var Encrypt = function(PlainAlphabet, CypherAlphabet, PlainText) {
+    var CypherText = ""; //We start with no letters in our output
+    var Position = 0;
+    while(Position < PlainText.length ) { 
+        var PlainTextLetter = PlainText[Position]; //The letter we want to Encypher this time
+        var LetterNum = PlainAlphabet.search(PlainTextLetter); //Finds what position our Letter is in our Alphabet
+        CypherText = CypherText + CypherAlphabet[LetterNum];//Look up that position in our Substitution Alphabet and add our Cypher Letter to the OutputText
+        Position = Position + 1;
     }
-    return OutputText; //Send it back
+    return CypherText; //Send it back
 };
 
-var Decrypt = function(Alphabet, SubstitutionAlphabet, CypherText) {
-    var OutputText = ""; //We start with no letters in our output
+var Decrypt = function(PlainAlphabet, CypherAlphabet, CypherText) {
+    var PlainText = ""; //We start with no letters in our output
     
     
     
-    return OutputText; //Send it back
+    return PlainText; //Send it back
 };
 
